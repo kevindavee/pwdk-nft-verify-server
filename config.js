af0f6565db3c8503117da61455bd47bec5159019
Kevin Dave <kevindavee1505@gmail.com>
2022-05-03 22:49:38 +0700

    create new signature

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports.web3Config = {
     rpcUrl: process.env.RPC_URL,
-    contractAddress: process.env.contractAddress,
+    contractAddress: process.env.CONTRACT_ADDRESS,
+    privateKey: process.env.PRIVATE_KEY
 };
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const { ethers } = require('ethers');
 const { web3Config } = require('./config');
 const abi = require('./abi.json');
+const { getNonCommonCountByTokenIds } = require('./metadata-repo');
 
 const rpcProvider = new ethers.providers.JsonRpcProvider(web3Config.rpcUrl);
 const nftContract = new ethers.Contract(web3Config.contractAddress, abi, rpcProvider);
@@ -12,12 +13,29 @@ const nftContract = new ethers.Contract(web3Config.contractAddress, abi, rpcProv
 const app = express();
 app.use(express.json());
 app.post('/verify', async (req, res) => {
-    const { address, signature } = req.body;
-    const tokensOfOwner = await nftContract.tokensOfOwner(address);
-    const stringifiedTokensOfOwner = JSON.stringify(tokensOfOwner.map(t => t.toNumber()));
-    const signerAddr = await ethers.utils.verifyMessage(stringifiedTokensOfOwner, signature);
-    if (signerAddr !== address) {
-        return res.status(403).json({ error: 'signature verification failed' });
+    try {
+        const { address, signature } = req.body;
+        const tokensOfOwner = await nftContract.tokensOfOwner(address);
+        const tokens = tokensOfOwner.map(t => t.toNumber());
+        const stringifiedTokensOfOwner = JSON.stringify(tokens);
+        const signerAddr = await ethers.utils.verifyMessage(stringifiedTokensOfOwner, signature);
+        if (signerAddr !== address) {
+            return res.status(403).json({ error: 'signature verification failed' });
+        };
+    
+        const rareTokenCount = getNonCommonCountByTokenIds(tokens);
+        if (rareTokenCount === 0) {
+            return res.status(403).json({ error: 'forbidden for verification' });
+        };
+    
+        const wallet = new ethers.Wallet(web3Config.privateKey);
+        const newSignature = await wallet.signMessage(stringifiedTokensOfOwner);
+        return res.status(200).json({ 
+            signature: newSignature
+        });
+    } catch (e) {
+        console.error(e.message);
+        return res.status(500).json({ error: 'internal server error' });
     }
 });
 
